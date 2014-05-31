a28a9bed2c41c33f6a3443a57652a469f1665f8f
Pedro Del Gallego <pedro.delgallego@gmail.com>
2014-05-31 23:25:20 +0200

    feat(security): add sign in and sign up screens

---
+++ InboxTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxTableViewController.h
+//  Ghostchat
+//
+//  Created by Pedro Del Gallego Vida on 31/05/14.
+//  Copyright (c) 2014 Cingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxTableViewController : UITableViewController
+
+@end
---
+++ InboxTableViewController.m
@@ -0,0 +1,44 @@
+//
+//  InboxTableViewController.m
+//  Ghostchat
+//
+//  Created by Pedro Del Gallego Vida on 31/05/14.
+//  Copyright (c) 2014 Cingo. All rights reserved.
+//
+
+#import "InboxTableViewController.h"
+
+@interface InboxTableViewController ()
+
+@end
+
+@implementation InboxTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+@end
