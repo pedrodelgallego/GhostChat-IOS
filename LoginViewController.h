0b84a9dc942a7dd4eba674b2138f67e336b70452
Pedro Del Gallego <pedro.delgallego@gmail.com>
2014-06-01 13:19:56 +0200

    feat(security): add sign up with parse.com

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ghostchat
+//
+//  Created by Pedro Del Gallego Vida on 01/06/14.
+//  Copyright (c) 2014 Cingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Ghostchat
+//
+//  Created by Pedro Del Gallego Vida on 01/06/14.
+//  Copyright (c) 2014 Cingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
